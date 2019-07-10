94b5466fba17e60a45f82452b262d826e03426d0
atharvapatil <atharvaabhaypatil@gmail.com>
2019-07-09 22:33:24 -0400

    app walk through phase one

---
+++ 02-app-walkthrough/02-app-walkthrough.framerfx/code/Page.tsx
@@ -0,0 +1,63 @@
+import * as React from "react"
+import { Override, Data } from "framer"
+
+const data = Data({
+    currentPage: 0,
+})
+
+const variants = {
+    enabled: { width: 13, height: 13, opacity: 0.7, background: "#000000" },
+    disabled: { width: 12, height: 12, opacity: 0.2, background: "#000000" },
+}
+
+export function Page(): Override {
+    return {
+        onChangePage(currentPage) {
+            data.currentPage = currentPage
+        },
+    }
+}
+
+export function circle1(): Override {
+    return {
+        variants: variants,
+        animate: data.currentPage === 0 ? "enabled" : "disabled",
+    }
+}
+
+export function circle2(): Override {
+    return {
+        variants: variants,
+        animate: data.currentPage === 1 ? "enabled" : "disabled",
+    }
+}
+
+export function circle3(): Override {
+    return {
+        variants: variants,
+        animate: data.currentPage === 2 ? "enabled" : "disabled",
+    }
+}
+
+export function forward(): Override {
+    return {
+        onChangePage(currentPage) {
+            data.currentPage = currentPage
+        },
+        onTap() {
+            if (data.currentPage >= 0 && data.currentPage < 2) {
+                data.currentPage = data.currentPage + 1
+            }
+        },
+    }
+}
+
+export function backward(): Override {
+    return {
+        onTap() {
+            if (data.currentPage > 0 && data.currentPage < 3) {
+                data.currentPage = data.currentPage - 1
+            }
+        },
+    }
+}
